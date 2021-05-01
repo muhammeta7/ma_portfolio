46c39fa07414ef0ba156fa8a111ca8e1217f130f
muhammeta7 <muhammeta7@gmail.com>
2021-05-01 12:32:11 -0400

    create Project model

--- portfolio/personal_portfolio/models.py
+++ portfolio/personal_portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+	title = models.CharField(max_length=100)
+	description = models.CharField(max_length=250)
+	image = models.ImageField(upload_to="personal_portfolio/images/")
+	github_url = models.URLField
+	heroku_url = models.URLField(blank=True)
